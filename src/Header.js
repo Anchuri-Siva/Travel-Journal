07a5380888e5f56e02064f888ce92225157d9f48
Siva Anchuri <158072420+Anchuri-Siva@users.noreply.github.com>
2025-12-05 10:48:12 +0530

    Spelling

--- src/Header.js
+++ src/Header.js
@@ -2,7 +2,7 @@ export default function Header(){
     return(
         <header>
             <img src="./glob.jpeg" alt=" " className="glob-img"></img>
-            <span>The Traval Journal</span>
+            <span>The Travel Journal</span>
         </header>
     )
 }
